f8c31ac4b19fa3db64a543237626c75b5a75dfb2
Brixter Porras <brixterporras@gmail.com>
2023-03-02 09:52:35 +0800

    feat: added day 01

---
+++ day01/target/debug/day01.d
@@ -0,0 +1,1 @@
+/home/brix/Repos/aoc-2022/day01/target/debug/day01: /home/brix/Repos/aoc-2022/day01/src/main.rs
---
+++ day01/target/debug/deps/day01-dfb66c893c57aaf9.d
@@ -0,0 +1,5 @@
+/home/brix/Repos/aoc-2022/day01/target/debug/deps/day01-dfb66c893c57aaf9: src/main.rs
+
+/home/brix/Repos/aoc-2022/day01/target/debug/deps/day01-dfb66c893c57aaf9.d: src/main.rs
+
+src/main.rs:
